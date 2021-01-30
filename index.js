7da29307acb9459d70bc6421ce69d74829666269
TAKURO FUKAMIZU <takuro.f.1987@gmail.com>
2021-01-30 09:50:42 +0900

    cli is working

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+
+const ASSET_PATH = 'assets/images/mermaid/';
+const FORMAT = 'png';
+
+const DEBUG = true;
+
+const processBlock = (block) => {
+  const source = block.body;
+  const hash = crypto.createHash('sha1').update(source).digest('hex');
+  const inputFilePath = path.join(ASSET_PATH, `${hash}.mmd`);
+  const outputFilePath = path.join(ASSET_PATH, `${hash}.${FORMAT}`);
+  fs.writeFileSync(inputFilePath, source, { coding: 'utf-8' });
+  const stdout = execSync(`npx mmdc -i ${inputFilePath} -o ${outputFilePath}`);
+  if (DEBUG) console.log(stdout);
+  return `<img src=/${outputFilePath}>`;
+};
+
 module.exports = {
   website: {
     assets: './dist',
@@ -12,8 +33,7 @@ module.exports = {
       if (lang != 'mermaid') { 
         return block;
       }
-      const scopedBody = `<div class="mermaid">${block.body}</div>`;
-      block.body = scopedBody;
+      block.body = processBlock(block);
       return block;
     }
   },
